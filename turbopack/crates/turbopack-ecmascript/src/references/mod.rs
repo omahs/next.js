7733e3b851a9055dd1575609aa11ce9b13721903
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-08-30 11:18:45 +0900

    Fix export src issue

--- turbopack/crates/turbopack-ecmascript/src/references/mod.rs
+++ turbopack/crates/turbopack-ecmascript/src/references/mod.rs
@@ -2685,7 +2685,10 @@ impl<'a> VisitAstPath for ModuleReferencesVisitor<'a> {
             .map(find_turbopack_part_id_in_asserts)
             .is_some();
 
-        if export.src.is_none() {
+        if match &export.src {
+            None => true,
+            Some(src) => src.value == "__TURBOPACK_PART__",
+        } {
             for spec in export.specifiers.iter() {
                 fn to_string(name: &ModuleExportName) -> &JsWord {
                     name.atom()
